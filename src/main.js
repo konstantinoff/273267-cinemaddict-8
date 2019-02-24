0ad1d65d9723d51d9e7e796297faf6fe7c297c7b
Аndrey <andrey.konstantinov1992@gmail.com>
2019-02-24 19:24:15 +0300

    make cards and filters template

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import addFilter from "./navigation_template";
+import addCard from "./film_card_template";
+
+const navBar = document.querySelector(`.main-navigation`);
+const filmContainer = document.querySelector(`.films-list__container`);
+const extraFilms = document.querySelectorAll(`.films-list--extra .films-list__container`);
+
+const renderCard = (dist, CARDS_AMOUNT = 7) => {
+  const films = new Array(CARDS_AMOUNT)
+    .fill()
+    .map(addCard);
+  dist.insertAdjacentHTML(`beforeend`, films.join(``));
+};
+
+const render = () =>{
+  navBar.insertAdjacentHTML(`afterbegin`, addFilter(`Favorites`, 8));
+  navBar.insertAdjacentHTML(`afterbegin`, addFilter(`History`, 4));
+  navBar.insertAdjacentHTML(`afterbegin`, addFilter(`Watchlist`, 13));
+  navBar.insertAdjacentHTML(`afterbegin`, addFilter(`All movies`, 25));
+  renderCard(filmContainer);
+  const filter = document.querySelectorAll(`.main-navigation__item`);
+  extraFilms.forEach((element) => renderCard(element, 2));
+  filter.forEach((element) => element.addEventListener(`click`, () => {
+    const RANDOM_CARDS_AMOUNT = Math.floor(Math.random() * 7) + 1;
+    filmContainer.innerHTML = ``;
+    renderCard(filmContainer, RANDOM_CARDS_AMOUNT);
+  }));
+};
+
+render();
---
+++ src/navigation_template.js
@@ -0,0 +1,1 @@
+export default (name, amount = ``) => `<a href="#" class="main-navigation__item">${name} <span class="main-navigation__item-count">${amount}</span></a>`;
