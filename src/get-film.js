c5bc387aa7e5bfcd09ca9a39273b7ec5c6170c93
Аndrey <andrey.konstantinov1992@gmail.com>
2019-03-04 21:06:05 +0300

    change constant name

--- src/get-film.js
+++ src/get-film.js
@@ -1,10 +1,10 @@
-const titleAmount = 1;
-const genreAmount = 1;
-const yearsRange = -50;
+const TITLE_AMOUNT = 1;
+const GENRE_AMOUNT = 1;
+const YEARS_RANGE = -50;
 const postersArray = [`accused`, `blackmail`, `blue-blazes`, `fuga-da-new-york`, `moonrise`, `three-friends`];
 const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`. `);
 const getRandomArrayElements = (array, elementsAmount) => Array.from(array).sort(() => 0.5 - Math.random()).slice(0, elementsAmount);
-const getRandomTimestamp = () => new Date(Date.now() + 1 + Math.floor(Math.random() * yearsRange) * 12 * 30 * 24 * 60 * 60 * 1000);
+const getRandomTimestamp = () => new Date(Date.now() + 1 + Math.floor(Math.random() * YEARS_RANGE) * 12 * 30 * 24 * 60 * 60 * 1000);
 const getRandomMark = () => (Math.random() * 10).toFixed(1);
 const getRandomFilmRange = () => Math.floor(Math.random() * 180) + 60;
 
@@ -22,7 +22,7 @@ const task = () => ({
     `Бойцовский клуб`,
     `Иван Васильевич меняет профессию`,
     `Жизнь прекрасна`,
-  ], titleAmount),
+  ], TITLE_AMOUNT),
   filmDescription: getRandomArrayElements(description, 3),
   filmRange: getRandomFilmRange(),
   filmMark: getRandomMark(),
@@ -33,7 +33,7 @@ const task = () => ({
     `Comedies`,
     `Crime`,
     `Dramas`,
-    `Epics`]), genreAmount),
+    `Epics`]), GENRE_AMOUNT),
   poster: `images/posters/${getRandomArrayElements(postersArray, 1)}.jpg`
 });
 
