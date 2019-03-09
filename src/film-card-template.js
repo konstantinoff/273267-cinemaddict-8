36b001278207ca7e0821b3e5e6b926127796b20f
Аndrey <andrey.konstantinov1992@gmail.com>
2019-03-09 23:30:44 +0300

    add OOP

--- src/film-card-template.js
+++ src/film-card-template.js
@@ -1,17 +1,42 @@
-export default (getFilm) => {
-  const randomYear = getFilm.filmDate.getFullYear();
-  const filmHours = Math.floor((getFilm.filmRange / 60));
-  const filmMinutes = getFilm.filmRange % 60;
-  return `<article class="film-card">
-          <h3 class="film-card__title">${getFilm.title}</h3>
-          <p class="film-card__rating">${getFilm.filmMark}</p>
+import createElement from './create-element';
+
+export default class GetFilmCard {
+  constructor({title, filmDescription, filmRange, filmMark, filmDate, genre, poster}) {
+    this._title = title;
+    this._description = filmDescription;
+    this._filmRange = filmRange;
+    this._filmMark = filmMark;
+    this._filmDate = filmDate;
+    this._genre = genre;
+    this._poster = poster;
+  }
+
+  _onEditButtonClick() {
+    return typeof this._onClick === `function` && this._onClick();
+  }
+  set onClick(fn) {
+    this._onClick = fn;
+  }
+  bind() {
+    this._element.querySelector(`.film-card__comments`)
+      .addEventListener(`click`, this
+        ._onEditButtonClick.bind(this));
+  }
+
+  get template() {
+    const year = this._filmDate.getFullYear();
+    const filmHours = Math.floor((this._filmRange / 60));
+    const filmMinutes = this._filmRange % 60;
+    return `<article class="film-card">
+          <h3 class="film-card__title">${this._title}</h3>
+          <p class="film-card__rating">${this._filmMark}</p>
           <p class="film-card__info">
-            <span class="film-card__year">${randomYear}</span>
+            <span class="film-card__year">${year}</span>
             <span class="film-card__duration">${filmHours}h ${filmMinutes}m</span>
-            <span class="film-card__genre">${getFilm.genre}</span>
+            <span class="film-card__genre">${this._genre[0]}</span>
           </p>
-          <img src="${getFilm.poster}" alt="" class="film-card__poster">
-          <p class="film-card__description">${getFilm.filmDescription.join(`. `)}</p>
+          <img src="${this._poster}" alt="" class="film-card__poster">
+          <p class="film-card__description">${this._description.join(`. `)}</p>
           <button class="film-card__comments">13 comments</button>
 
           <form class="film-card__controls">
@@ -20,4 +45,12 @@ export default (getFilm) => {
             <button class="film-card__controls-item button film-card__controls-item--favorite"><!--Mark as favorite-->FAV</button>
           </form>
         </article>`;
-};
+  }
+  render() {
+    this._element = createElement(this.template);
+    this.bind();
+    return this._element;
+  }
+}
+
+
